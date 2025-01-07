9245f9e67ba271105424c0b6c1c3c0996e7178ef
Vitor Cosso <vitorcosso@gmail.com>
2025-01-07 11:45:13 -0300

    feat: creating new Constructor for Consumer and UNIFORM_STRING to deserializer class

--- src/main/java/org/example/utils/KafkaProperties.java
+++ src/main/java/org/example/utils/KafkaProperties.java
@@ -2,9 +2,12 @@ package org.example.utils;
 
 import java.util.Properties;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import static org.example.utils.UNIFORM_STRING.KAFKA_SERIALIZER_CLASS_CONFIG;
+import static org.example.utils.UNIFORM_STRING.KAFKA_DESERIALIZER_CLASS_CONFIG;
 import static org.example.utils.UNIFORM_STRING.KAFKA_SERVER_PORT;
+
 
 public class KafkaProperties {
 
@@ -12,15 +15,23 @@ public class KafkaProperties {
 
     public KafkaProperties(){
         this.kafkaProperties = new Properties();
+    }
+
+    public void createKafkaPropertiesProducer(){
         this.kafkaProperties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER_PORT);
         this.kafkaProperties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KAFKA_SERIALIZER_CLASS_CONFIG);
         this.kafkaProperties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KAFKA_SERIALIZER_CLASS_CONFIG);
     }
 
+    public void createKafkaPropertiesConsumer(){
+        this.kafkaProperties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER_PORT);
+        this.kafkaProperties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KAFKA_DESERIALIZER_CLASS_CONFIG);
+        this.kafkaProperties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KAFKA_DESERIALIZER_CLASS_CONFIG);
+    }
 
     public void add_properties(Object object_propertie, Object propertie){
         this.kafkaProperties.setProperty((String) object_propertie, (String) propertie);
     }
 
-    public Properties getKafkaProperties(){ return kafkaProperties; }
+    public Properties getProperties(){ return kafkaProperties; }
 }
--- src/main/java/org/example/utils/UNIFORM_STRING.java
+++ src/main/java/org/example/utils/UNIFORM_STRING.java
@@ -1,9 +1,11 @@
 package org.example.utils;
 
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 public class UNIFORM_STRING {
     public static final String KAFKA_SERVER_PORT = "127.0.0.1:9092";
     public static final String KAFKA_SERIALIZER_CLASS_CONFIG = StringSerializer.class.getName();
+    public static final String KAFKA_DESERIALIZER_CLASS_CONFIG = StringDeserializer.class.getName();
     public static final String TOPPIC_NAME = "ECOMMERCE_NEW_ORDER";
 }
