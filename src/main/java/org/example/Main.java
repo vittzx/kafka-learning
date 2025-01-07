080c8ef4c7c92bae08d021d422ad416dc9206e58
Vitor Cosso <vitorcosso@gmail.com>
2025-01-07 11:46:59 -0300

    feat: creating KafkaConsumerService

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.services.KafkaConsumerService;
 import org.example.services.KafkaMessageService;
 
 import java.time.LocalDateTime;
@@ -9,11 +10,19 @@ import java.util.concurrent.ExecutionException;
 public class Main {
 
     private static final KafkaMessageService kafkaMessageService = new KafkaMessageService();
+    private static final KafkaConsumerService kafkaConsumerService = new KafkaConsumerService();
+
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-
-        System.out.println("KAFKA MESSAGE CONTROLLER START");
-        kafkaMessageService.sendMessage("PEDIDO_010992_COSSO_SPECIAL,PRODUTO_TESTE_15,RS1000.00, " + LocalDateTime.now());
-        System.out.println("KAFKA MESSAGE CONTRLLER FINISH");
+        sendMessageMain();
     }
+
+
+    public static void sendMessageMain() throws ExecutionException, InterruptedException{
+        System.out.println("KAFKA MESSAGE CONTROLLER START");
+        kafkaMessageService.sendMessage("ULTIMA_MESSAGE,PRODUTO_TESTE_15,RS1000.00, " + LocalDateTime.now());
+        System.out.println("KAFKA MESSAGE CONTROLLER FINISHED");
+    }
+
+
 }
--- src/main/java/org/example/services/KafkaConsumerService.java
+++ src/main/java/org/example/services/KafkaConsumerService.java
@@ -1,4 +1,64 @@
 package org.example.services;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.example.utils.KafkaProperties;
+
+import java.time.Duration;
+import java.util.Collections;
+
+import static org.example.utils.UNIFORM_STRING.TOPPIC_NAME;
+
 public class KafkaConsumerService {
+
+    private final KafkaConsumer<String, String> consumer;
+
+    public KafkaConsumerService(){
+        KafkaProperties kafkaProperties = new KafkaProperties();
+        kafkaProperties.createKafkaPropertiesConsumer();
+        kafkaProperties.add_properties(ConsumerConfig.GROUP_ID_CONFIG, KafkaMessageService.class.getSimpleName());
+        this.consumer = new KafkaConsumer<>(kafkaProperties.getProperties());
+    }
+
+    public void consumerTopic(String topicName){
+        System.out.println("STARTING CONSUMER TOPIC " + topicName + " MESSAGES");
+
+        consumer.subscribe(Collections.singletonList(topicName));
+        boolean condition = true;
+
+        while(condition) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(5000));
+
+            if (records.isEmpty()) {
+                System.out.println("None message found. Continues");
+                continue;
+            }
+
+            System.out.println("MESSAGE FOUND");
+
+            for(var record: records){
+                System.out.println("--------- NEW MESSAGE ---------");
+                System.out.println("MESSAGE: " + record.topic());
+                System.out.println("KEY: " + record.key());
+                System.out.println("VALUE: " + record.value());
+                System.out.println("PARTITION: " + record.partition());
+                System.out.println("OFFSET: " + record.offset());
+            }
+
+
+
+            if(records.count() == 3){ condition = false; }
+        }
+        System.out.println("FINISHING CONSUMER TOPIC " + topicName + " MESSAGES");
+    }
+
+    public static void main(String[] args){
+        final KafkaConsumerService kafkaConsumerService = new KafkaConsumerService();
+        System.out.println("KAFKA CONSUMER CONTROLLER START");
+        kafkaConsumerService.consumerTopic(TOPPIC_NAME);
+        System.out.println("KAFKA CONSUMER CONTROLLER FINISHED");
+    }
+
+
 }
